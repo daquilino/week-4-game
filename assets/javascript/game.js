bb5e388794d130ef423cf423581b45b2162d203b
douglas aquilino <douglas aquilino>
2017-02-05 22:57:22 -0500

    Basic HTML,CSS done. Need to complete JS logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,27 @@
 /*  Douglas Aquilino       February 11, 2017   */
-/*  RCB - "Psychic-Game" Homework #4           */
+/*  RCB - "Crystals Collector" Homework #4           */
 /*                game.js                      */
 
 
 	// Global Variable Declarations
+
+
+
+
+
+
+
+	// When "Show Instructions" button clicked. Instructions display.
+	$("#show").on("click",function(){
+
+		$("#instructions-col").slideDown(1000);	//1000 milisecond delay
+
+	});
+
+
+	//When "Hide Instructions" button clicked. Instructions hide.
+	$("#hide").on("click",function(){
+
+		$("#instructions-col").slideUp(1000);	//1000 milisecond delay
+
+	});
